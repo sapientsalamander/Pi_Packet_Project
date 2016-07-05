64832862335515aef71e7547b8559ad5009a643c
Caitlyn Cook <cpcook4@gmail.com>
2016-07-05 16:43:39 -0700

    Added automatic start, file lock.

--- sender/send.py
+++ sender/send.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
-import time
+
+import time, fcntl, sys
+
+#Lock to only allow one instance of this program to run
+pid_file = '/tmp/send.pid'
+fp = open(pid_file, 'w')
+try:
+   fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+except IOError:
+   print 'An instance of this program is already running'
+   sys.exit(0)
 
 from scapy.all import *
 import Adafruit_CharLCD as LCD
@@ -7,6 +17,7 @@ import Adafruit_CharLCD as LCD
 lcd = LCD.Adafruit_CharLCDPlate()
 lcd.set_color(0,0,0)
 
+pkt = IP(dst='10.0.24.243')/UDP(sport=7777,dport=7777)/("Hello World!")
 number_packets_sent = 0
 select_just_pressed = False
 
@@ -15,14 +26,14 @@ def print_lcd():
    lcd.clear()
    lcd.message('# of packets\nsent: ' + str(number_packets_sent))
 
-if __name__ == '__main__':
-   print_lcd()
-   while(True):
-      if lcd.is_pressed(LCD.SELECT):
-         if not select_just_pressed:
-            select_just_pressed=True
-            number_packets_sent += 1
-            send(IP(dst='10.0.24.243')/UDP(dport=7777)/ICMP()/"Hello World!", iface="eth0")
-            print_lcd()
-      else:
-         select_just_pressed = False
+print_lcd()
+
+while(True):
+   if lcd.is_pressed(LCD.SELECT):
+      if not select_just_pressed:
+         select_just_pressed=True
+         number_packets_sent += 1
+         send(pkt, iface='eth0')
+         print_lcd()
+   else:
+      select_just_pressed = False
--- sender/send_as_fast_as_posssible.py
+++ sender/send_as_fast_as_posssible.py
@@ -7,20 +7,13 @@ import Adafruit_CharLCD as LCD
 lcd = LCD.Adafruit_CharLCDPlate()
 lcd.set_color(0,0,0)
 
-number_packets_sent = 0
-select_just_pressed = False
+packet = IP(dst='10.0.24.243')/UDP(sport=7777,dport=7777)/("Hello World!")
 
-#packet = IP(dst='10.0.24.243')/UDP(sport=7777,dport=7777)/("Hello World!")
+#s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+#s.bind(('eth0',0))
+#packet = Ether()/IP(dst='10.0.24.243')/ICMP()
+#data = packet.build()
 
-s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-s.bind(('eth0',0))
-packet = Ether()/IP(dst='10.0.24.243')/ICMP()
-data = packet.build()
-
-def print_lcd():
-   lcd.clear()
-   lcd.message('# of packets\nsent: ' + str(number_packets_sent))
-
-if __name__ == '__main__':
-   while(True):
-      s.send(data)
+while(True):
+   send(packet)
+   time.sleep(0.5)
