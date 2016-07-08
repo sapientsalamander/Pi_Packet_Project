aa016425e7cd68024359317c6800c1483a4a5bf0
Kamal Sadek <kamaljalals@gmail.com>
2016-07-07 17:01:21 -0700

    Implement multiple screens
    Currently, there is one screen for a summary of the network, and one for the payload

--- receiver/receive.py
+++ receiver/receive.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import fcntl, sys
+import fcntl
+import sys
 
 # Lock to only allow one instance of this program to run
 # Opens (and creates if non-existent) a file in /tmp/, and attempts to lock
@@ -16,10 +17,26 @@ except IOError:
    sys.exit(0)
 # End of lock code.
 
-import socket, time, thread
+import socket
+import time
+import thread
 
 from scapy.all import *
 import Adafruit_CharLCD as LCD
+
+class Screens:
+   """An enum class for the different screens.
+
+   Screens:
+      Summary: Number of total packets received and current bandwidth.
+      Payload: The payload of the previous packet that came in.
+   """
+   Summary, Payload = range(2)
+
+
+cur_screen = Screens.Summary
+
+screen_output = [['',''], ['','']]
 
 # Abbreviations for bandwidth measuring.
 BDWTH_ABBRS = ('bps', 'Kbps', 'Mbps')
@@ -27,6 +44,12 @@ BDWTH_ABBRS = ('bps', 'Kbps', 'Mbps')
 # An LCD lock, to ensure that the two threads, one for port listening and
 # one for bandwidth measuring, do not interfere when updating the LCD.
 lcd_lock = thread.allocate_lock()
+
+def display_loop():
+   while True:
+      print_line(screen_output[cur_screen][0], 0)
+      print_line(screen_output[cur_screen][1], 1)
+      time.sleep(1)
 
 def print_line(message, line):
    """Print message to screen on line, multithreading safe.
@@ -66,17 +89,14 @@ def update_packet_info(packet, number_packets_received):
       packet: A capture scapy packet.
       number_packets_received: Packets received since program started.
    """
-   message = ('Rx:%3d ' % number_packets_received)
+   screen_output[Screens.Summary][0] = ('Rx:%3d ' % number_packets_received)
 
    # Grabs the payload from the scapy packet. If none is available,
    # or if packet is not a scapy packet, then it cannot parse the payload.
-   if packet:
-      try:
-         message += packet.getlayer(Raw).load
-      except AttributeError:
-         message += 'No payload'
-
-   print_line(message, 0)
+   try:
+      screen_output[Screens.Payload][0] = packet.getlayer(Raw).load
+   except AttributeError:
+      screen_output[Screens.Payload][0] = 'No payload'
 
 def update_statistics():
    """Calculate the bandwidth and displays it on the LCD."""
@@ -98,8 +118,7 @@ def update_statistics():
       # Ex. Bw: 30.5 Kbps.
       message = 'Bw:%5.1f %s' % (bandwidth, BDWTH_ABBRS[i])
 
-      print_line(message, 1)
-
+      screen_output[Screens.Summary][1] = message
       rx_prev = rx_cur
       time_prev = time_cur
 
@@ -115,19 +134,27 @@ def listen_packets():
       packet = sniff(filter = 'port 7777', count = 1)[0]
 
       number_packets_received += 1
-
       update_packet_info(packet, number_packets_received) 
+
+def input_loop():
+   global cur_screen
+   while True:
+      if lcd.is_pressed(LCD.UP):
+         cur_screen = Screens.Summary
+      elif lcd.is_pressed(LCD.DOWN):
+         cur_screen = Screens.Payload
 
 if __name__ == '__main__':
    # Initializes LCD and turn off LED.
    lcd = LCD.Adafruit_CharLCDPlate()
    lcd.set_color(0,0,0)
 
-   print_line('Awaiting packets', 0)
+   screen_output[Screens.Summary][0] = 'Awaiting packets'
 
    try:
+      thread.start_new_thread(display_loop, ())
       thread.start_new_thread(update_statistics, ())
+      thread.start_new_thread(input_loop, ())
    except:
       print 'Error: ', sys.exc_info()[0]
-
    listen_packets()
